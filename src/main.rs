cf75c3442fccd150d367d581fbaa15bfcf672a41
Ryan Garrett <ryan@thepinkhacker.com>
2023-07-14 22:34:04 -0400

    Fix command parsing

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,35 @@
 use clap::Parser;
 use device_query::{DeviceState, DeviceQuery};
-use std::{process::{Command, Stdio}, thread};
+use shlex::split;
+use std::process::Command;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[clap(num_args = 1..)]
-    command: Vec<String>,
-}
-
-fn screensaver_runner(args: Args) {
-    let command = args.command
-        .get(0)
-        .expect("Invalid command length of 0.");
-    let mut screensaver_command = Command::new(command);
-    for arg in args.command.iter().skip(1) {
-        screensaver_command.arg(arg);
-    }
-    screensaver_command.stdout(Stdio::inherit())
-        .output()
-        .expect("Failed to run screensaver command");
+    command: String,
 }
 
 fn main() {
     let args = Args::parse();
-    let screensaver_handle = thread::spawn(move || screensaver_runner(args));
+    let command_split = split(&args.command)
+        .expect("Failed to parse command");
+    let command = command_split.get(0)
+        .expect("Invalid command length of 0.");
+    let mut screensaver_command = Command::new(command);
+    for arg in command_split.iter().skip(1) {
+        screensaver_command.arg(arg);
+    }
+    let mut screensaver_child = screensaver_command.spawn()
+            .expect("Failed to run screensaver command.");
 
     let device_state = DeviceState::new();
     let inital_mouse_position = device_state.get_mouse().coords;
     loop {
-        // Exit if screensaver is done running
-        if screensaver_handle.is_finished() {
-            break;
-        }
-
         let mouse = device_state.get_mouse();
         if inital_mouse_position != mouse.coords {
             break;
         }
     }
+    screensaver_child
+        .kill()
+        .expect("Failed to stop screensaver process");
 }
